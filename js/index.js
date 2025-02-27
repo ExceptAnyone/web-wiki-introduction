385dfc4213fb93107aba13cc38a03ca3d6e41966
장정안 <wjddks96@naver.com>
2025-02-27 14:20:55 +0900

    feat: 댓글 입력 구현

--- js/index.js
+++ js/index.js
@@ -1,19 +1,67 @@
+//@ts-check
 console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
+  '%c' +
+    ' __      __  ______   __  __   ______     ' +
+    '\n' +
+    '/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    ' +
+    '\n' +
     "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
+    '\n' +
+    ' \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    ' +
+    '\n' +
+    '  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ ' +
+    '\n' +
+    '   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ ' +
+    '\n' +
     "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
+  'color: #d81b60; font-size: 16px; font-weight: bold;'
 );
 
-console.log("알맞은 스크립트를 작성하세요");
+function qs(selector, scope = document) {
+  if (!selector) throw 'no selector';
+
+  return scope.querySelector(selector);
+}
+
+const commentInput = qs('.comment-input', document);
+const commentList = qs('#comment-list');
+const commentCancelButton = qs('.comment-cancel-button');
+const commentSubmitButton = qs('.comment-submit-button');
+
+function handleCommentFormSubmit(event) {
+  event.preventDefault();
+
+  const commentContent = commentInput.value;
+  if (!commentContent.trim()) return;
+
+  const commentTemplate = `
+    <div class="comment-item">
+      <div class="comment-author">
+        <img
+          src="./images/comment-author-icon.png"
+          alt="사용자 프로필 이미지"
+        />
+        <span>방문자</span>
+      </div>
+      <div class="comment-content">
+        ${commentContent}
+      </div>
+    </div>
+  `;
+
+  const commentItem = document.createElement('li');
+  commentItem.innerHTML = commentTemplate;
+  commentList.appendChild(commentItem);
+
+  commentInput.value = '';
+}
+
+function handleCancelButtonClick() {
+  commentInput.value = '';
+  console.log('cancel');
+}
+
+commentCancelButton.addEventListener('click', handleCancelButtonClick);
+commentSubmitButton.addEventListener('click', handleCommentFormSubmit);
+
+commentSubmitButton.addEventListener('submit', handleCommentFormSubmit);
